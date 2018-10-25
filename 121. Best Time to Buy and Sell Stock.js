a4e1cf793f0ca639aa4eb6add703f1f4fd69a210
yanyj1993 <yanyj1993@gmail.com>
2018-10-25 14:12:01 +0800

    resolve [ALL Pass]

---
+++ 121. Best Time to Buy and Sell Stock.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+
+    let maxProfit = 0;
+
+    let length = prices.length;
+
+    let max = -1;
+
+    for(let i = 0; i < length; i ++) {
+        max = prices[i];
+        for(let j = i + 1; j < length; j ++) {
+           if(prices[j] > max) {
+               max = prices[j];
+           }
+        }
+
+        maxProfit = maxProfit > (max - prices[i]) ? maxProfit : (max - prices[i]);
+    }
+
+
+    return maxProfit > 0 ? maxProfit : 0;
+
+
+};
+
+console.log(maxProfit([3,2,1]));
