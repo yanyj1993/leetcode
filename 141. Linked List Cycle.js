a73b7c3af9b1510e38f24912d45c613389efc9f1
yanyj <yanyj@nj-strongit.com.cn>
2018-11-26 17:13:12 +0800

    done && all pass

---
+++ 141. Linked List Cycle.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+
+    if(head === null || head.next === null) {
+        return false;
+    }
+
+    let fast = head.next;
+    let slow = head;
+
+    while(fast != slow) {
+        if(fast == null || fast.next === null) {
+            return false
+        }
+
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+
+    return true;
+
+};
